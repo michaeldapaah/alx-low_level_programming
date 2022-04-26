61dd406901760050b259b7c06aae0bea12a06d57
michaeldapaah <michaeldapaah0@gmail.com>
2022-04-26 07:56:37 -0700

    memory pointers and arrays task 100

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -11,3 +11,19 @@ void set_string(char **s, char *to)
 {
 *s = to;
 }
+
+100-set_string.c
+
+#include "holberton.h"
+
+/**
+  * set_string - Sets the value of a pointer to char
+  * @s: the value to modify
+  * @to: the value to assign
+  *
+  * Return: Nothing.
+  */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
